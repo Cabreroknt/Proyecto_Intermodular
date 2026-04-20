fb1aca1e3f25c61cfd1052066ee579c6c66f93f0
Cabreroknt <unaxgonzarodri@gmail.com>
2026-04-20 14:34:32 +0200

    Enums

--- src/main/java/dam/code/models/utils/EstadoBuque.java
+++ src/main/java/dam/code/models/utils/EstadoBuque.java
@@ -1,4 +1,19 @@
 package dam.code.models.utils;
 
 public enum EstadoBuque {
+    EN_PUERTO("En Puerto"),
+    NAVEGANDO("Navegando"),
+    FONDEADO("Fondeado"),
+    EN_REPARACION("En Reparación"),
+    DESARMADO("Fuera de Servicio");
+
+    private final String mensaje;
+
+    EstadoBuque(String mensaje) {
+        this.mensaje = mensaje;
+    }
+
+    public String getMensaje() {
+        return mensaje;
+    }
 }
--- src/main/java/dam/code/models/utils/Rol.java
+++ src/main/java/dam/code/models/utils/Rol.java
@@ -1,4 +1,21 @@
 package dam.code.models.utils;
 
 public enum Rol {
+    CAPITAN("Capitán"),
+    OFICIAL_PUENTE("Oficial de Puente"),
+    CONTRAMAESTRE("Contramaestre"),
+    MARINERO("Marinero"),
+    JEFE_MAQUINAS("Jefe de Máquinas"),
+    COCINERO("Cocinero"),
+    ADMINISTRADOR("Admin de Puerto"); // Rol especial para AdminView
+
+    private final String descripcion;
+
+    Rol(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
 }
